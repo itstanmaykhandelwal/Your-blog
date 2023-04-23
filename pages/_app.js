04d741b191c5222eed01fc1a19e4e19ac92e0c3c
Tanmay Khandelwal <iamtanmaykh07@gmail.com>
2023-04-23 17:54:06 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
+import React,{useState,useEffect} from 'react';
+import 'tailwindcss/tailwind.css'
+import '../styles/global.scss'
+import { Layout } from '../components';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+        <Component {...pageProps} />
+    </Layout>
+  )
 }
---
+++ services/index.js
@@ -0,0 +1,9 @@
+import {request, gql} from 'graphql-request'; 
+
+export const getPost = async () => {
+    const query = gql`
+        query MyQuery{
+            
+        }
+    `
+}
